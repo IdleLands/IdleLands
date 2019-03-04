2366352b589bc1fadac4dead7fe4b6a01d4c2e02
seiyria <kyle@seiyria.com>
2019-03-03 20:44:08 -0600

    interface cleanup for socket events

--- src/shared/interfaces/ServerEvent.ts
+++ src/shared/interfaces/ServerEvent.ts
@@ -1,5 +1,7 @@
 
 export enum ServerEventName {
+  GameMessage = 'gamemessage',
+
   AuthSignIn = 'auth:signin'
 }
 
--- src/shared/models/ServerSocketEvent.ts
+++ src/shared/models/ServerSocketEvent.ts
@@ -1,4 +1,10 @@
+import { ServerEventName } from '../interfaces';
 
 export class ServerSocketEvent {
+
+  protected gameError(err: string) {
+    this.socket.emit(ServerEventName.GameMessage, { message: err, type: 'error' });
+  }
+
   constructor(private socket) {}
 }
