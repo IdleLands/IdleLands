19bcab94f4acad90dd4a84bf6db4487ceb8d8e4c
Kyle J. Kemp <kyle@seiyria.com>
2019-07-10 14:20:02 -0500

    can reorder pets

--- src/client/app/petslist/petslist.page.ts
+++ src/client/app/petslist/petslist.page.ts
@@ -1,25 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocketClusterService } from '../socket-cluster.service';
 import { GameService } from '../game.service';
 import { AlertController } from '@ionic/angular';
 import { ServerEventName } from '../../../shared/interfaces';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-petslist',
   templateUrl: './petslist.page.html',
   styleUrls: ['./petslist.page.scss'],
 })
-export class PetslistPage implements OnInit {
+export class PetslistPage implements OnInit, OnDestroy {
+
+  public petOrder: any[] = [];
+  public petHash: any = {};
+  public pets$: Subscription;
 
   constructor(
     private router: Router,
+    private storage: Storage,
     private alertCtrl: AlertController,
     private socketService: SocketClusterService,
     public gameService: GameService
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.petOrder = (await this.storage.get('petOrder')) || [];
+
+    this.pets$ = this.gameService.player$.subscribe(player => {
+      if(!player) return;
+
+      this.petHash = player.$petsData.allPets;
+      this.checkForNewPetsAndAddToList(this.petHash);
+    });
+  }
+
+  async ngOnDestroy() {
+    if(this.pets$) this.pets$.unsubscribe();
+  }
+
+  async reorderPets($event) {
+    $event.detail.complete(this.petOrder);
+    await this.storage.set('petOrder', this.petOrder);
+  }
+
+  public checkForNewPetsAndAddToList(pets) {
+    const numPetsEarned = Object.keys(pets).length;
+
+    if(numPetsEarned !== this.petOrder.length) {
+      Object.keys(pets).forEach(petKey => {
+        if(this.petOrder.includes(petKey)) return;
+
+        this.petOrder.push(petKey);
+      });
+    }
   }
 
   activate(petType: string) {
