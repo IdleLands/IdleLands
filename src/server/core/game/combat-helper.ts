367e7886c49dbf90c16d33325fd0fd45892c771e
seiyria <kyle@seiyria.com>
2019-07-29 10:08:49 -0500

    update helper to add players to combat correctly

--- src/server/core/game/combat-helper.ts
+++ src/server/core/game/combat-helper.ts
@@ -15,21 +15,44 @@ export class CombatHelper {
 
   createAndRunCombat(player: Player) {
 
-    // copy party name, grab all members
-    if(player.$party) {
-      console.log(player.$party);
-    }
-
     // if no party, just make a random name for this single person
     const characters = {};
     const parties = {};
 
-    const currentPartyId = 0;
-    const currentCharacterId = 0;
+    // player party
+    if(player.$party) {
+      parties[0] = { id: 0, name: player.$party.name };
+    } else {
+      parties[0] = { id: 0, name: this.assets.party() };
+    }
 
-    // TODO: get all players, parties, assign ids, and start combat
+    // monster party
+    parties[1] = { id: 1, name: this.assets.party() };
+
+    // give players ids
+    let currentId = 0;
 
     const playerParty = this.getAllPartyMembers(player);
+    playerParty.forEach(combatPlayer => {
+      combatPlayer.combatId = currentId;
+      combatPlayer.combatPartyId = 0;
+      characters[currentId] = combatPlayer;
+      currentId++;
+    });
+
+    // figure out the average party level for monster generation
+    const averagePartyLevel = Math.floor(playerParty.reduce((prev, cur) => prev + cur.level, 0) / playerParty.length);
+
+    // generate monsters to fight against
+    const monsters = [];
+    for(let i = 0; i < playerParty.length; i++) {
+      const monster = this.createCombatMonster(averagePartyLevel);
+      monster.combatId = currentId;
+      monster.combatPartyId = 1;
+
+      monsters.push(monster);
+      currentId++;
+    }
 
     const combat: ICombat = {
       name: this.assets.battle(),
@@ -50,8 +73,8 @@ export class CombatHelper {
     return players.map(partyPlayer => this.createCombatCharacter(partyPlayer));
   }
 
-  private createCombatMonster(): ICombatCharacter {
-    return null;
+  private createCombatMonster(level: number): ICombatCharacter {
+    return this.assets.createBattleMonster(level);
   }
 
   private createCombatCharacter(player: Player): ICombatCharacter {
--- src/server/core/game/events/Battle.ts
+++ src/server/core/game/events/Battle.ts
@@ -7,6 +7,8 @@ export class Battle extends Event {
   public static readonly WEIGHT = 18;
 
   public operateOn(player: Player) {
+    player.$$game.combatHelper.createAndRunCombat(player);
+    
     this.emitMessage([player], 'You should be battling right now, but it is not implemented.', AdventureLogEventType.Meta);
   }
 }
