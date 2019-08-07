4096b1ea89ffc65665e6a745c5dcbc827013a743
Kyle J. Kemp <kyle@seiyria.com>
2019-08-07 13:39:51 -0500

    fix lint

--- src/shared/models/entity/Player.entity.ts
+++ src/shared/models/entity/Player.entity.ts
@@ -712,7 +712,7 @@ export class Player implements IPlayer {
 
   public syncPremium() {
     if(!this.$premiumData) return;
-    
+
     this.$statistics.set('Game/Contributor/ContributorTier', this.$statistics.get('Game/Contributor/ContributorTier'));
 
     const tier = this.$premiumData.tier;
