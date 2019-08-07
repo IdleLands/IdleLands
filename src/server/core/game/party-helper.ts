dbb9f6d10d8711c8a3390df8442738efc89be1f6
Kyle J. Kemp <kyle@seiyria.com>
2019-08-07 13:36:18 -0500

    remove console

--- src/server/core/game/party-helper.ts
+++ src/server/core/game/party-helper.ts
@@ -49,7 +49,6 @@ export class PartyHelper {
     party.members.push(player.name);
     player.increaseStatistic('Event/Party/Join', 1);
 
-    console.log(party.members[0], player.name);
     if(party.members[0] !== player.name && player.$personalities.has('Telesheep')) {
       const leader = this.playerManager.getPlayer(party.members[0]);
       player.setPos(leader.x, leader.y, leader.map, leader.region);
