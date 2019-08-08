9d3a52a6eef8f1ab0e575d9d1b71eb8e48e7be3c
Kyle J. Kemp <kyle@seiyria.com>
2019-08-08 16:17:35 -0500

    add more ads

--- src/server/core/game/events/TownCrier.ts
+++ src/server/core/game/events/TownCrier.ts
@@ -5,12 +5,17 @@ import { AdventureLogEventType } from '../../../../shared/interfaces';
 const townCrierMessages = [
   {
     message: 'Got questions? Are some aspects of IdleLands confusing? Worry not, check out the FAQ!',
-    link: 'https://github.com/IdleLands/IdleLands/wiki/FAQ',
+    link: 'https://idle.land/docs/faq/',
+    origin: 'system'
+  },
+  {
+    message: 'Need help? There\'s a lot of docs on idle.land, check them out!',
+    link: 'https://idle.land/docs/home/',
     origin: 'system'
   },
   {
     message: 'Found a bug? Can you reproduce a bug I can\'t? Great! Get ILP or Gold for giving me some reproduction steps!',
-    link: 'https://github.com/IdleLands/IdleLands/wiki/Bug-Bounties',
+    link: 'https://idle.land/docs/bug-bounties/',
     origin: 'system'
   },
   {
@@ -19,12 +24,32 @@ const townCrierMessages = [
     origin: 'system'
   },
   {
+    message: 'Interested in contributing content or maps to IdleLands? Check out the Github!',
+    link: 'https://github.com/IdleLands',
+    origin: 'system'
+  },
+  {
+    message: 'Want to join our community on reddit? Head on over to /r/idle_lands!',
+    link: 'https://www.reddit.com/r/idle_lands/',
+    origin: 'system'
+  },
+  {
+    message: 'Want to keep up with the game? Follow @IdleLands on Twitter!',
+    link: 'https://twitter.com/IdleLands',
+    origin: 'system'
+  },
+  {
+    message: 'Want to keep up with the game? Follow @IdleLands on Facebook!',
+    link: 'https://facebook.com/IdleLands',
+    origin: 'system'
+  },
+  {
     message: 'Want to support the game even more? Support Seiyria on Patreon!',
     link: 'https://www.patreon.com/seiyria',
     origin: 'system'
   },
   {
-    message: `Interested in what Seiyria has been up to lately?
+    message: `Interested in Seiyria's other projects?
     Check out Land of the Rair! It's not an idle game, but rather is a fully playable (alpha) MORPG.`,
     link: 'https://rair.land',
     origin: 'system'
