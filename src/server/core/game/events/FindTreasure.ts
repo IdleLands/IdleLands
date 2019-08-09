d94d35cd21e4bb502d0504e41bd39f7cf0b370a2
Kyle J. Kemp <kyle@seiyria.com>
2019-08-09 09:12:01 -0500

    treasure chest cooldown 5minu

--- src/server/core/game/events/FindTreasure.ts
+++ src/server/core/game/events/FindTreasure.ts
@@ -1,16 +1,29 @@
 import { Event, EventName } from './Event';
 import { Player } from '../../../../shared/models/entity';
 import { Item } from '../../../../shared/models';
-import { ItemClass } from '../../../../shared/interfaces';
+import { ItemClass, AdventureLogEventType } from '../../../../shared/interfaces';
 
 export class FindTreasure extends Event {
   public static readonly WEIGHT = 0;
 
   public operateOn(player: Player, opts: any = { treasureName: '' }) {
 
+    player.increaseStatistic('Treasure/Total/Touch', 1);
+
+    const curTimer = player.cooldowns[opts.treasureName];
+    if(Date.now() < curTimer) {
+      player.increaseStatistic('Treasure/Total/Empty', 1);
+      this.emitMessage([player],
+        `You could not loot ${opts.treasureName} because it was empty! Check back at ${new Date(curTimer)}.`,
+        AdventureLogEventType.Explore);
+      return;
+    }
+
+    // 30 minute cooldown
+    player.cooldowns[opts.treasureName] = Date.now() + (30 * 60 * 1000);
+
     const { chests, items } = this.assetManager.allTreasureAssets;
 
-    player.increaseStatistic('Treasure/Total/Touch', 1);
     player.increaseStatistic(`Treasure/Chest/${opts.treasureName}`, 1);
 
     const treasureItems = chests[opts.treasureName].items;
