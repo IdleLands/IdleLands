ad7fb9f4b40f09ac3fe76f33b7ced578a069f2ab
seiyria <kyle@seiyria.com>
2019-08-10 15:06:47 -0500

    gold current val is not a rn

--- src/client/app/petcurrent/reroll.popover.ts
+++ src/client/app/petcurrent/reroll.popover.ts
@@ -9,19 +9,19 @@ import { ServerEventName } from '../../../shared/interfaces';
     <ion-list>
       <ion-list-header>Actions</ion-list-header>
 
-      <ion-item (click)="rerollName()" button [disabled]="gameService.playerRef.gold.__current < 50000">
+      <ion-item (click)="rerollName()" button [disabled]="gameService.playerRef.gold < 50000">
         Reroll Name (50k)
       </ion-item>
 
-      <ion-item (click)="rerollGender()" button [disabled]="gameService.playerRef.gold.__current < 10000">
+      <ion-item (click)="rerollGender()" button [disabled]="gameService.playerRef.gold < 10000">
         Reroll Gender (10k)
       </ion-item>
 
-      <ion-item (click)="rerollAffinity()" button [disabled]="gameService.playerRef.gold.__current < 100000">
+      <ion-item (click)="rerollAffinity()" button [disabled]="gameService.playerRef.gold < 100000">
         Reroll Affinity (100k)
       </ion-item>
 
-      <ion-item (click)="rerollAttribute()" button [disabled]="gameService.playerRef.gold.__current < 75000">
+      <ion-item (click)="rerollAttribute()" button [disabled]="gameService.playerRef.gold < 75000">
         Reroll Attribute (75k)
       </ion-item>
     </ion-list>
