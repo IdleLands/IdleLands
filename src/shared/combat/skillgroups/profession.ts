fab7206dfc58ce28f06fa9869130d5c7f958c148
Kyle J. Kemp <kyle@seiyria.com>
2019-08-11 09:04:34 -0500

    chance bug

--- src/shared/combat/skillgroups/profession.ts
+++ src/shared/combat/skillgroups/profession.ts
@@ -798,9 +798,10 @@ export const ProfessionSkillMap: { [key in Profession]: ICombatWeightedSkillChoi
       skills: (combat, caster) => {
 
         // 1+ because otherwise if it does (min, max) and they're both 0 it throws
+        const min = caster.stats[Stat.INT] * 0.1;
         const damage = combat.chance.integer({
-          min: caster.stats[Stat.INT] * 0.1,
-          max: 1 + (caster.stats[Stat.INT] * 0.5)
+          min,
+          max: Math.max(min + 1, (caster.stats[Stat.INT] * 0.5))
         });
 
         return [
