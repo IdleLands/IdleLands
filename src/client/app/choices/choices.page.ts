49d7a27e7b48717b7e97c3e5c766500543a58f99
Kyle J. Kemp <kyle@seiyria.com>
2019-08-12 11:10:16 -0500

    closes #38

--- src/client/app/choices/choices.page.ts
+++ src/client/app/choices/choices.page.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GameService } from '../game.service';
 import { SocketClusterService } from '../socket-cluster.service';
 import { IChoice, ServerEventName } from '../../../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-choices',
   templateUrl: './choices.page.html',
   styleUrls: ['./choices.page.scss'],
 })
-export class ChoicesPage {
+export class ChoicesPage implements OnInit, OnDestroy {
+
+  private choices$: Subscription;
+  public choices: IChoice[] = [];
 
   constructor(
     private socketService: SocketClusterService,
     public gameService: GameService
   ) { }
+
+  public ngOnInit() {
+    this.choices$ = this.gameService.player$.subscribe(player => {
+      if(!player.$choicesData) return;
+      this.choices = player.$choicesData.choices;
+    });
+  }
+
+  public ngOnDestroy() {
+    if(this.choices$) this.choices$.unsubscribe();
+  }
 
   public choose(choice: IChoice, valueChosen: string) {
     this.socketService.emit(ServerEventName.ChoiceMake, { choiceId: choice.id, valueChosen });
