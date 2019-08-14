b7f3da7fcb8867fdbc949d715debd4e5d146734f
seiyria <kyle@seiyria.com>
2019-08-14 17:59:53 -0500

    display fix for collectibles

--- src/client/app/game.service.ts
+++ src/client/app/game.service.ts
@@ -191,10 +191,13 @@ export class GameService {
       {
         pathSearch: '$collectibles',
         flag: false,
-        order: (collObj) => sortBy(Object.values(collObj.collectibles), [
-          (coll: ICollectible) => -coll.foundAt,
-          (coll: ICollectible) => coll.name
-        ]),
+        order: (collObj) => {
+          const allcolls = Object.values(collObj.collectibles);
+          const found = allcolls.filter((x: ICollectible) => x.foundAt);
+          const unfound = allcolls.filter((x: ICollectible) => !x.foundAt);
+
+          return sortBy(found, 'name').concat(sortBy(unfound, 'name'));
+        },
         observable: this.collectibles,
         playerData: (pl) => pl.$collectiblesData
       },
