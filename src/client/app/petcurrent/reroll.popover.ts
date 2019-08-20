3a7d6325cf19215b013f14b4446d0a8ea69a4022
Kyle J. Kemp <kyle@seiyria.com>
2019-08-19 21:06:27 -0500

    add icon to each reroll action

--- src/client/app/petcurrent/reroll.popover.ts
+++ src/client/app/petcurrent/reroll.popover.ts
@@ -10,19 +10,19 @@ import { ServerEventName } from '../../../shared/interfaces';
       <ion-list-header>Actions</ion-list-header>
 
       <ion-item (click)="rerollName()" button [disabled]="gameService.playerRef.gold < 50000">
-        Reroll Name (50k)
+        <ion-icon slot="start" name="refresh"></ion-icon> Reroll Name (50k)
       </ion-item>
 
       <ion-item (click)="rerollGender()" button [disabled]="gameService.playerRef.gold < 10000">
-        Reroll Gender (10k)
+        <ion-icon slot="start" name="refresh"></ion-icon> Reroll Gender (10k)
       </ion-item>
 
       <ion-item (click)="rerollAffinity()" button [disabled]="gameService.playerRef.gold < 100000">
-        Reroll Affinity (100k)
+        <ion-icon slot="start" name="refresh"></ion-icon> Reroll Affinity (100k)
       </ion-item>
 
       <ion-item (click)="rerollAttribute()" button [disabled]="gameService.playerRef.gold < 75000">
-        Reroll Attribute (75k)
+        <ion-icon slot="start" name="refresh"></ion-icon> Reroll Attribute (75k)
       </ion-item>
     </ion-list>
   `,
