beb3a4da608473f7b09f72a3d18a69211d1620e2
boozledorf <boozledorf@users.noreply.github.com>
2019-08-21 09:04:13 -0400

    oocAbility return structure

--- src/server/core/game/professions/Barbarian.ts
+++ src/server/core/game/professions/Barbarian.ts
@@ -51,7 +51,7 @@ export class Barbarian extends BaseProfession implements IProfession {
   public oocAbility(player: Player): string {
     player.$$game.eventManager.doEventFor(player, EventName.BattlePvP);
     this.emitProfessionMessage(player, `You seek out a worthy opponent!`);
-    return `You seek out a worthy opponent!`;
+    return {success: true, message: `You seek out a worthy opponent!`};
   }
 
   public determineStartingSpecial(): number {
