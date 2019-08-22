6c49120580ff67acafae0f49d3f354b523dfdedc
seiyria <kyle@seiyria.com>
2019-08-22 07:53:53 -0500

    does pathing fix it

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 download('IdleLands/Custom-Assets', 'assets/content', () => {});
 download('IdleLands/Maps',          'assets/maps', () => {});
 
-fs.writeFileSync('src/client/assets/tiles.png', fs.readFileSync('assets/maps/img/tiles.png'));
+fs.writeFileSync('./src/client/assets/tiles.png', fs.readFileSync('./assets/maps/img/tiles.png'));
