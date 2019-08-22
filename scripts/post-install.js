dc138f2ba7664f9850f8dbc8eb253bb71fa0f33e
seiyria <kyle@seiyria.com>
2019-08-22 07:54:59 -0500

    ok callbacks are good

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,6 +2,6 @@ const download = require('download-github-repo');
 const fs = require('fs');
 
 download('IdleLands/Custom-Assets', 'assets/content', () => {});
-download('IdleLands/Maps',          'assets/maps', () => {});
-
-fs.writeFileSync('./src/client/assets/tiles.png', fs.readFileSync('./assets/maps/img/tiles.png'));
+download('IdleLands/Maps',          'assets/maps', () => {
+  fs.writeFileSync('./src/client/assets/tiles.png', fs.readFileSync('./assets/maps/img/tiles.png'));
+});
