16c3119a48da553de2329358950bd4314a6b61ca
seiyria <kyle@seiyria.com>
2019-08-22 06:41:43 -0500

    move sprites primarily to maps repo

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,4 +1,7 @@
 const download = require('download-github-repo');
+const fs = require('fs');
 
 download('IdleLands/Custom-Assets', 'assets/content', () => {});
 download('IdleLands/Maps',          'assets/maps', () => {});
+
+fs.writeFileSync('src/client/assets/tiles.png', fs.readFileSync('assets/maps/img/tiles.png'));
