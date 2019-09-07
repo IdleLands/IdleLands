c30ca731514f675bd8713be8edbde183f32e7fce
Bright Pattern <trammellduke@gmail.com>
2019-09-06 21:26:26 -0500

    Added festival aggregate stats function and call in ngOnInit()

--- src/client/app/festivals/festivals.page.ts
+++ src/client/app/festivals/festivals.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { sortBy } from 'lodash';
+import { sortBy, assignInWith } from 'lodash';
 
 import { GameService } from '../game.service';
 import { map } from 'rxjs/operators';
 import { IFestival } from '../../../shared/interfaces';
+import { Festivals } from 'src/shared/models';
 
 @Component({
   selector: 'app-festivals',
@@ -15,6 +16,8 @@ export class FestivalsPage implements OnInit {
 
   public isLoaded: boolean;
   public festivals: IFestival[] = [];
+  public aggregate: any;
+
 
   constructor(
     private http: HttpClient,
@@ -26,12 +29,28 @@ export class FestivalsPage implements OnInit {
       .pipe(map((x: any) => x.festivals))
       .subscribe(festivals => {
         this.festivals = sortBy(
-          festivals.filter(x => this.isFestivalValid(x)),
+          festivals.filter((x: IFestival) => this.isFestivalValid(x)),
           fest => !fest.startedBy.includes(this.gameService.playerRef.name)
         );
-
+        this.aggregate = this.festivalAggregate(this.festivals);
         this.isLoaded = true;
       });
+  }
+
+  public festivalAggregate(festivals: IFestival[]): any {
+    const aggStats = { stats: {} }
+
+    for (let o of festivals) {
+      for (let [k, v] of Object.entries(o.stats)) {
+        if (!aggStats.stats[k]) {
+          aggStats.stats[k] = v
+        } else {
+          aggStats.stats[k] += v
+        }
+      }
+    }
+
+    return aggStats;
   }
 
   public isFestivalValid(festival: IFestival): boolean {
