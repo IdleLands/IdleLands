3bd72f00e911646b3cf34eada632fdd2b6c922c3
boozledorf <boozledorf@users.noreply.github.com>
2019-09-14 16:19:45 -0400

    Rebalance Alchemize

--- src/server/core/game/attributes/Alchemist.ts
+++ src/server/core/game/attributes/Alchemist.ts
@@ -18,17 +18,17 @@ export class Alchemist extends BaseAttribute implements IAttribute {
       if(xpTaken === 0) return { success: false, message: 'The experiment was a failure; there is no experience to be taken.' };
 
       player.$pets.$activePet.gainXP(-xpTaken, false);
-      player.$pets.$activePet.gainGold(xpTaken, false);
+      player.$pets.$activePet.gainGold(xpTaken * xpTaken, false);
 
-      return { success: true, message: `Your pet has gained ${xpTaken.toLocaleString()} gold!` };
+      return { success: true, message: `Your pet has gained ${(xpTaken * xpTaken).toLocaleString()} gold!` };
     }
 
     const goldTaken = player.$pets.$activePet.gold.total;
-    player.$pets.$activePet.gainXP(goldTaken, false);
+    player.$pets.$activePet.gainXP(Math.floor(Math.sqrt(goldTaken)), false);
     player.$pets.$activePet.gainGold(-goldTaken, false);
 
     if(goldTaken === 0) return { success: false, message: 'The experiment was a failure; there is no gold to be taken.' };
 
-    return { success: true, message: `Your pet has gained ${goldTaken.toLocaleString()} exp!` };
+    return { success: true, message: `Your pet has gained ${Math.floor(Math.sqrt(goldTaken)).toLocaleString()} exp!` };
   }
 }
