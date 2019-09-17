8ba9b5ce43c87d36da8dca154748a112a8225f57
Kyle J. Kemp <kyle@seiyria.com>
2019-09-17 15:33:09 -0500

    raid bosses more hp

--- src/server/core/game/combat-helper.ts
+++ src/server/core/game/combat-helper.ts
@@ -475,7 +475,7 @@ export class CombatHelper {
     const stats = base.stats;
 
     Object.keys(stats).forEach(stat => {
-      const mult = stat === Stat.HP ? 1000 : 100;
+      const mult = stat === Stat.HP ? 10000 : 100;
 
       stats[stat] += players.length * mult;
     });
