1be7e14e17959191e63de0942231bbaa8ce58751
Kain <drkain@outlook.com>
2019-09-23 13:11:44 +1200

    Smaller variable?!
    Dammit travis

--- src/server/core/game/guild-manager.ts
+++ src/server/core/game/guild-manager.ts
@@ -274,9 +274,8 @@ export class GuildManager {
   public raidBossRewards(level: number) {
     if(level % 50 !== 0 || level < 100) return [];
 
-    const now = new Date();
-    const dayOfYear = (Date.UTC(now.getFullYear(),now.getMonth(),now.getDate()) - Date.UTC(now.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
-
+    const n = new Date();
+    const dayOfYear = (Date.UTC(n.getFullYear(), n.getMonth(), n.getDate()) - Date.UTC(n.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
     const tier = ((level - 100) / 50) + 1;
     const rng = new Chance(level + ' ' + dayOfYear);
 
