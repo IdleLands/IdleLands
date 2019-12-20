6b634274af6dc21e3d849e252024eddb4e1db4b1
Kyle J. Kemp <kyle@seiyria.com>
2019-12-20 08:22:37 -0600

    Update Fancypants.ts

--- src/server/core/game/personalities/Fancypants.ts
+++ src/server/core/game/personalities/Fancypants.ts
@@ -2,7 +2,7 @@ import { Personality } from '../../../../shared/interfaces';
 
 export class Fancypants extends Personality {
   static description = `You will automatically purchase items, double down on gambling events,
- and sell found items. Any Yes/No choices will be be chosen randomly`;
+ and sell found items. Any Yes/No choices will be be chosen randomly.`;
   static statMultipliers = { };
   static toggleOff = ['Affirmer', 'Indecisive', 'Denier'];
 }
