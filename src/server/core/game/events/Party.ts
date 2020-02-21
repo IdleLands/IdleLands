05e44a3120e65afaae6ded94761b53e6134db881
boozledorf <boozledorf@users.noreply.github.com>
2020-02-21 11:41:02 -0500

    Hardcore Party exclusion
    Prevents Hardcore players from joining or forming parties

--- src/server/core/game/events/Party.ts
+++ src/server/core/game/events/Party.ts
@@ -12,6 +12,11 @@ export class Party extends Event {
         'You almost started looking for a party before you realized you did not want one!', AdventureLogEventType.Party);
       return;
     }
+    
+    if(player.hardcore) {
+      this.emitMessage([player], 'You looked for a party, but you are too Hardcore for anyone to join you!', AdventureLogEventType.Party);
+      return;
+    }
 
     if(player.$party) {
       this.emitMessage([player],
@@ -28,6 +33,7 @@ export class Party extends Event {
       && !x.$personalities.isActive('Solo')
       && !x.$personalities.isActive('Camping')
       && !x.$personalities.isActive('Leader')
+      && !x.hardcore
     );
     if(checkPlayers.length < 3) {
       this.emitMessage([player], 'You almost found enough people for a group!', AdventureLogEventType.Party);
