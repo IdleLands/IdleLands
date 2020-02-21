e10d209e33a3c17d92e26d68a54407189bd78453
boozledorf <boozledorf@users.noreply.github.com>
2020-02-21 17:00:31 -0500

    Send message to chat on hardcore character death

--- src/server/core/game/combat-helper.ts
+++ src/server/core/game/combat-helper.ts
@@ -341,7 +341,13 @@ export class CombatHelper {
 
         playerRef.increaseStatistic(statistic, value);
         
-        if(playerRef.hardcore && data.winningParty !== 0 && !data.wasTie) playerRef.$statistics.set(`Hardcore/Dead`, 1);
+        if(playerRef.hardcore && data.winningParty !== 0 && !data.wasTie) {
+          playerRef.$statistics.set(`Hardcore/Dead`, 1);
+          playerRef.$$game.chatHelper.sendMessageFromClient({
+            message: `Hardcore player ${playerRef.name} has passed on.`,
+            playerName: '☆System'
+          });
+        }
       }
     });
 
