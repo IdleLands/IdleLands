e807bf0de39b3d976598cecfcb74e471bae5f2ec
seiyria <kyle@seiyria.com>
2020-03-29 09:44:40 -0500

    remove recursion from checkStaminaTick

--- src/shared/models/entity/Player.entity.ts
+++ src/shared/models/entity/Player.entity.ts
@@ -458,26 +458,29 @@ export class Player implements IPlayer {
   }
 
   private checkStaminaTick() {
-    if(this.stamina.atMaximum()) {
-      this.nextStaminaTick = Date.now();
+    do {
+
+      if(this.stamina.atMaximum()) {
+        this.nextStaminaTick = Date.now();
         if(this.$personalities.isActive('Restless')) {
-        this.oocAction(2);
-      return;
+          this.oocAction(2);
+          return;
+        }
       }
-    }
 
-    if(this.stamina.atMaximum() || Date.now() < this.nextStaminaTick) return;
+      if(this.stamina.atMaximum() || Date.now() < this.nextStaminaTick) return;
 
-    this.increaseStatistic('Character/Stamina/Gain', 1);
-    this.stamina.add(1);
+      this.increaseStatistic('Character/Stamina/Gain', 1);
+      this.stamina.add(1);
 
-    this.nextStaminaTick = this.nextStaminaTick + (STAMINA_TICK_BOOST * (this.$premiumData && this.$premiumData.tier ? 0.8 : 1));
+      // TODO: figure out the delta #times instead of doing a loop
+      this.nextStaminaTick = this.nextStaminaTick + (STAMINA_TICK_BOOST * (this.$premiumData && this.$premiumData.tier ? 0.8 : 1));
 
-    if(this.$personalities.isActive('Restless') && this.stamina.atMaximum() && Date.now() < this.nextStaminaTick) {
-      this.oocAction(2);
-    }
+      if(this.$personalities.isActive('Restless') && this.stamina.atMaximum() && Date.now() < this.nextStaminaTick) {
+        this.oocAction(2);
+      }
 
-    if(Date.now() > this.nextStaminaTick) this.checkStaminaTick();
+    } while(Date.now() > this.nextStaminaTick);
   }
 
   private calculateStamina() {
